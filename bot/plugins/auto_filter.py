f608d1adc140ea93d4c79c443b72fb6fb1f488ac
Lucifer Moring Star <90172375+Lucifermoringsta@users.noreply.github.com>
2021-09-09 19:30:26 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -86,7 +86,7 @@ async def auto_filter(bot, update):
             file_size = "" if file_size == ("[0 B]") else file_size
             
             # add emoji down below inside " " if you want..
-            button_text = f"😈{file_size}📒{file_name}"
+            button_text = f"⭕️{file_size}📒{file_name}"
             
 
             if file_type == "video":
@@ -158,7 +158,7 @@ async def auto_filter(bot, update):
         
         # Just A Decaration
         result[0].append([
-            InlineKeyboardButton(f"📂 𝙿𝚊𝚐𝚎 1/{len_result if len_result < max_pages else max_pages} 📂", callback_data="ignore")
+            InlineKeyboardButton(f"🔷 𝙿𝚊𝚐𝚎 1/{len_result if len_result < max_pages else max_pages} 🔷", callback_data="ignore")
         ])
         
         
@@ -209,7 +209,7 @@ async def auto_filter(bot, update):
             await bot.send_photo(
                 chat_id = update.chat.id,
                 photo= MASSAGE_PHOTO,
-                caption=f"<b>📒Total File :- {(len_results)} </b>\n<b>🎬Movie Name :-</b> <code>{query}</code>",
+                caption=f"<b>⭕️Total File :- {(len_results)} </b>\n<b>🔥Movie Name :-</b> <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
